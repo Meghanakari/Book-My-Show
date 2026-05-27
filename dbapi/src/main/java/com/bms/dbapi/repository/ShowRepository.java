67d6af10eff544fce7651e5e690ac0cac1e0fc3b
Kari Meghana <karimeghana02@gmail.com>
2026-05-27 13:47:41 +0530

    done

--- dbapi/src/main/java/com/bms/dbapi/repository/ShowRepository.java
+++ dbapi/src/main/java/com/bms/dbapi/repository/ShowRepository.java
@@ -1,15 +1,11 @@
 package com.bms.dbapi.repository;
 
-import com.bms.dbapi.models.Show;
+import com.bms.dbapi.models.Theather;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.UUID;
 
 @Repository
-public interface ShowRepository extends JpaRepository<Show, UUID> {
-    @Query(value = "select * from shows where hall_id=:hallId", nativeQuery = true)
-    public List<Show> getShowsByHallId(UUID hallId);
+public interface TheatherRepositorty extends JpaRepository<Theather, UUID> {
 }
